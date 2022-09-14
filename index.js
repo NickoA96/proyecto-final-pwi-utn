9835cf8d982440a7bc422bc3f4987a3411d6dffc
Nico <nicolas.albornoz996@gmail.com>
2022-09-14 14:08:09 -0300

    actualizacion proyecto final3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const hbs = require('hbs');
 const mysql = require('mysql2');
 const nodemailer = require('nodemailer');
 const app = express();
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 80;
 
 
 
